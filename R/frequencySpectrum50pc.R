8dea26ec56589ae654be3cd5c8e0742fd9a94f95
Ed Baker <edwbaker@gmail.com>
2017-08-20 21:18:33 +0100

    Namespace functions.

--- R/frequencySpectrum50pc.R
+++ R/frequencySpectrum50pc.R
@@ -1,7 +1,7 @@
 frequencySpectrum50pc <- function(wave, plot=FALSE) {
-  s <- meanspec(wave, norm=TRUE, PSD=TRUE, plot=plot)
+  s <- seewave::meanspec(wave, norm=TRUE, PSD=TRUE, plot=plot)
   s50 <- s[(s[,2]) >= 0.5,]
-  s50_range <- c(s50[[1,1]], fpeaks(s, nmax=1, plot=FALSE)[1,1], s50[[nrow(s50),1]])
+  s50_range <- c(s50[[1,1]], seewave::fpeaks(s, nmax=1, plot=FALSE)[1,1], s50[[nrow(s50),1]])
   if (plot == TRUE) {
     abline(v=s50_range, col="red")
   }
