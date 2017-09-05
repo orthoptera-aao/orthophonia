ebaf8c04875b8360f79dde57296268c162eae1d1
Ed Baker <edwbaker@gmail.com>
2017-09-05 22:29:36 +0100

    Check build fails if test fails.

--- tests/testthat/test-bandwidth.R
+++ tests/testthat/test-bandwidth.R
@@ -3,6 +3,7 @@ test_that("bandwidth_of_sinewave_is_zero", {
   #Even though sine wave has bandwidth=0 meanspec() will have a small bandwidth
   a <- orthophonia::frequencySpectrumPowerQuartiles(testsound, 0, FALSE)
   expect_lt(a[[2]]-a[[1]], 0.05)
+  expect_equal(2,3)
 })
 
 #Test that min_freq > sine freq fails cleanly
