0b51778991161dce43504b69b2b03550e6dbba11
Ed Baker <edwbaker@gmail.com>
2017-09-07 14:33:46 +0100

    Updates.

--- R/analysis.R
+++ R/analysis.R
@@ -1,6 +1,9 @@
 bandwidth <- function(wave, min_freq=1000, plot = FALSE, method = "quartile") {
   if (method == "quartile") {
     a <- orthophonia::frequencySpectrumPowerQuartiles(wave, min_freq, plot)
+    if (is.null(a)) {
+      return(NULL)
+    }
     return (a[[2]] - a[[1]])
   }
 }
--- tests/testthat/test-bandwidth.R
+++ tests/testthat/test-bandwidth.R
@@ -4,7 +4,7 @@ context("Bandwidth")
 
 tests <- jsonlite::fromJSON("https://raw.githubusercontent.com/orthoptera-aao/orthophonia-tests/master/bandwidth.json")
 for (i in 1:length(tests)) {
-  test_that(tests[[i]]$desc_en, {
+  test_that(tests[[i]]$desc$en, {
     if (tests[[i]]$r_testsound[[1]] == "func") {
       testsound <- eval(parse(text=tests[[i]]$r_testsound[[2]]))
     }
@@ -17,7 +17,7 @@ for (i in 1:length(tests)) {
       result <- do.call(tests[[i]]$orthophonia_func, args = args)
       for (j in 1:length(tests[[i]]$tests)) {
         test_type = names(tests[[i]]$tests[j])
-        do.call(test_type, list(result, eval(parse(text=tests[[i]]$tests[[j]][[1]]))))
+        do.call(test_type, list(result, tests[[i]]$tests[[j]]))
       }
     } else {
       warning("Could not create a test file: ", tests[[i]]$desc_en)
